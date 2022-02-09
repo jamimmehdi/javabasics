13303f29f8afd22eba94f1ed265547ce493fa170
Jamim Mehdi <jamimmehdi@gmail.com>
2022-02-09 20:02:02 +0530

    9feb updated

--- src/com/javabasics/practice.java
+++ src/com/javabasics/practice.java
@@ -3,5 +3,49 @@ package com.javabasics;
 import java.util.Scanner;
 
 public class practice {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = s.nextInt();
+        System.out.println("Enter second number: ");
+        int b = s.nextInt();
 
+
+        System.out.println("What operation you want to perform?: +, -, /, %, *, table");
+        String operand = s.next();
+
+
+        switch (operand) {
+            case "+": {
+                System.out.println(a + b);
+                break;
+            }
+            case "-": {
+                System.out.println(a - b);
+                break;
+            }
+            case "*": {
+                System.out.println(a * b);
+                break;
+            }
+            case "/": {
+                System.out.println(a / b);
+                break;
+            }
+            case "%": {
+                System.out.println(a % b);
+                break;
+            }
+            case "table":{
+                int n = s.nextInt();
+                for (int i = 1; i <= 10; i++) {
+                    System.out.println(n + " x " + i + " = "+n*i);
+                }
+                break;
+            }
+            default: {
+                System.out.println("Enter a valid operand.");
+            }
+        }
+    }
 }
